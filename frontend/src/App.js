abfb2f9ed2c9573fffda17f14573bb626ffcb96d
Sazad Ahemad <mail2sazad@gmail.com>
2023-09-12 01:39:11 +0530

    Fixed errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // importing components
--- frontend/src/components/dashboard.component.js
+++ frontend/src/components/dashboard.component.js
@@ -13,7 +13,7 @@ export const Dashboard = () => {
         } else {
             navigate('/login');
         }
-    }, []);
+    }, [user]);
     
     return(
         <div>
